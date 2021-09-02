23c7b6e3aa2080bf5d34384339eebfabb2674595
Chris <ceochrism@mail.com>
2021-09-02 14:56:44 -0700

    Allowing the user to enter their own expressions and solve them.  If the equation has variables in it, the program will ask for values for the variables.  The variables are allowed to be repeated in the program (i.e the equation a+2-3*a/b).

--- ExpressionTreeCalculator/ExpressionTreeCalculator/Program.cs
+++ ExpressionTreeCalculator/ExpressionTreeCalculator/Program.cs
@@ -6,11 +6,58 @@ namespace ExpressionTreeCalculator
     {
         static void Main(string[] args)
         {
-            ExpressionTree tree = ExpressionTree.CreateExpression("a +b-c*a/(c-b) + a * 2 +1 + 2^2^(1+1)");
+            //ExpressionTree tree = ExpressionTree.CreateExpression("a +b-c*a/(c-b) + a * 2 +1 + 2^2^(1+1)");
 
-            var result = tree.SolveEquation();
-            Console.WriteLine();
-            Console.WriteLine($"The result for the given equation and variables is: {result}");
+            //var result = tree.SolveEquation();
+            //Console.WriteLine();
+            //Console.WriteLine($"The result for the given equation and variables is: {result}");
+
+            bool exit = false;
+            while(!exit)
+            {
+                Console.WriteLine("Enter The Expression You Would Like To Compute:");
+                try
+                {
+                    ExpressionTree tree = ExpressionTree.CreateExpression(Console.ReadLine());
+
+                    bool solve = true;
+                    while(solve)
+                    {
+                        Console.WriteLine("Solving the expression.");                        
+                        var result = tree.SolveEquation();
+                        Console.WriteLine();
+                        Console.WriteLine($"The result for the given equation and variables is: {result}");
+
+                        string exitSolve = "";
+                        while (exitSolve != "1" && exitSolve != "2")
+                        {
+                            Console.WriteLine("Type 1 to solve the equation again or Type 2 to genter a new expression/exit");
+                            exitSolve = Console.ReadLine();
+                        }
+                        if(exitSolve == "2")
+                        {
+                            solve = false;
+                        }
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Error creating that expression.");
+                }
+
+                string restart = "";
+                while (restart != "1" && restart != "2")
+                {
+                    Console.WriteLine("Type 1 to compute a new expression or Type 2 to exit.");
+                    restart = Console.ReadLine();
+                }
+                if(restart == "2")
+                {
+                    exit = true; 
+                }
+
+            }
 
         }
     }
